fffb66615662e3e2d95e9df02fc5c68d83c0b498
Sumanth Hagalavadi Gopalakrishna <hagalavadigopalakr.s@husky.neu.edu>
2017-09-20 15:53:19 -0400

    Homework Assignment 2 part 1 code

---
+++ Homework_Assignment_2/src/Business/AirplaneDetails.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Sumanth
+ */
+public class AirplaneDetails {
+    private String airplaneName;
+    private int yearOfMan;
+    private int numOfSeats;
+    private double serNo;
+    private double modelNo;
+    private int manufacturer;
+    private String timeOfFleetCat;
+    private String maintCertExp;
+    private String availablity;
+    private String airportName;
+
+    public String getAirplaneName() {
+        return airplaneName;
+    }
+
+    public void setAirplaneName(String airplaneName) {
+        this.airplaneName = airplaneName;
+    }
+
+    public int getYearOfMan() {
+        return yearOfMan;
+    }
+
+    public void setYearOfMan(int yearOfMan) {
+        this.yearOfMan = yearOfMan;
+    }
+
+    public int getNumOfSeats() {
+        return numOfSeats;
+    }
+
+    public void setNumOfSeats(int numOfSeats) {
+        this.numOfSeats = numOfSeats;
+    }
+
+    public double getSerNo() {
+        return serNo;
+    }
+
+    public void setSerNo(double serNo) {
+        this.serNo = serNo;
+    }
+
+    public double getModelNo() {
+        return modelNo;
+    }
+
+    public void setModelNo(double modelNo) {
+        this.modelNo = modelNo;
+    }
+
+    public int getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(int manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getTimeOfFleetCat() {
+        return timeOfFleetCat;
+    }
+
+    public void setTimeOfFleetCat(String timeOfFleetCat) {
+        this.timeOfFleetCat = timeOfFleetCat;
+    }
+
+    public String getMaintCertExp() {
+        return maintCertExp;
+    }
+
+    public void setMaintCertExp(String maintCertExp) {
+        this.maintCertExp = maintCertExp;
+    }
+
+    public String getAvailablity() {
+        return availablity;
+    }
+
+    public void setAvailablity(String availablity) {
+        this.availablity = availablity;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
+    }
+    
+}
---
+++ Homework_Assignment_2/src/Business/AirplaneDetailsHistory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sumanth
+ */
+public class AirplaneDetailsHistory {
+    private ArrayList<AirplaneDetails> airDetHist;
+    
+    public AirplaneDetailsHistory(){
+        airDetHist = new ArrayList<AirplaneDetails>();
+    }
+
+    public ArrayList<AirplaneDetails> getAirDetHist() {
+        return airDetHist;
+    }
+
+    public void setAirDetHist(ArrayList<AirplaneDetails> airDetHist) {
+        this.airDetHist = airDetHist;
+    }
+}
